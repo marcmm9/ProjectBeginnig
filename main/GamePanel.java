84ed8885c044941b510fb16397b4b787730f1695
marcm <marc.marvin.matern@gmail.com>
2025-03-08 21:43:29 +0100

    second version

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,8 +1,72 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     //Settings
-    final int originalTitelSize = 16;
+    final int originalTitleSize = 16;
+    final int scale = 3;
+
+    final int titleSize = originalTitleSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = titleSize * maxScreenCol;
+    final int screenHeight = maxScreenRow * titleSize;
+
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null){
+//            System.out.println("Game is Running");
+            update();
+            repaint();
+        }
+    }
+
+    public void update(){
+
+        if(keyHandler.upPressed)
+            playerY -= playerSpeed;
+        else if(keyHandler.downPressed)
+            playerY += playerSpeed;
+        else if(keyHandler.leftPressed)
+            playerX -= playerSpeed;
+        else  if(keyHandler.rightPressed)
+            playerX += playerSpeed;
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(playerX, playerY, titleSize, titleSize);
+        g2d.dispose();
+    }
 }
+
--- main/Main.java
+++ main/Main.java
@@ -1,4 +1,24 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame(); //neues Frame erstellen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //x beendet das Programm und minimiert es nicht nur
+        frame.setResizable(false); //Fenster lässt sich nicht in der Größe ändern
+        frame.setTitle("Der Anfang"); //Titel des Fensters
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
 }
